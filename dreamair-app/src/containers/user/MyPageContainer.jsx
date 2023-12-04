b2677c00a5cb29ff0920dcf6af72d5c8ba08ed03
yunazz2 <z____yn@naver.com>
2023-12-04 14:23:59 +0900

    ❄

--- dreamair-app/src/containers/user/MyPageContainer.jsx
+++ dreamair-app/src/containers/user/MyPageContainer.jsx
@@ -5,12 +5,12 @@ const MyPageContainer = () => {
     return (
         <>
             <div className="mypage_menu">
-                <a href="/user/update">회원정보 수정</a>
-                <a href="/user/checkin">체크인</a>
-                <a href="/user/mileage">마일리지 조회</a>
-                <a href="/user/deleteaccount">회원 탈퇴</a>
-                <a href="/user/mybooking">나의 탑승권 관리</a>
-                <a href="/user/logout">로그아웃</a>
+                <p><a href="/user/update">회원정보 수정</a></p>
+                <p><a href="/user/checkin">체크인</a></p>
+                <p><a href="/user/mileage">마일리지 조회</a></p>
+                <p><a href="/user/deleteaccount">회원 탈퇴</a></p>
+                <p><a href="/user/mybooking">나의 탑승권 관리</a></p>
+                <p><a href="/user/logout">로그아웃</a></p>
             </div>
         </>
     );

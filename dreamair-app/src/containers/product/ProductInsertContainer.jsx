d5ac550c6e11093001e1958094f0aace2cbe3172
seulgi9256 <sgij56@gmail.com>
2023-12-06 12:50:02 +0900

    2023.12.06

--- dreamair-app/src/containers/product/ProductInsertContainer.jsx
+++ dreamair-app/src/containers/product/ProductInsertContainer.jsx
@@ -8,19 +8,18 @@ const ProductInsertContainer = () => {
   const navigate = useNavigate()
 
   const onInsert = async (productId, routeNo, name, productCat, productPrice, departure, destination, description, unitsInStock) => {
-
-    const Product = {
-      productId,
-      routeNo, 
-      name, 
-      productCat, 
-      productPrice, 
-      departure, 
-      destination, 
-      description, 
-      unitsInStock,
-    }
     try {
+      const Product = {
+        productId,
+        routeNo, 
+        name, 
+        productCat, 
+        productPrice, 
+        departure, 
+        destination, 
+        description, 
+        unitsInStock,
+      }
       const response = await products.product_insert(Product) 
       alert('등록 완료')
       console.log(response.data);

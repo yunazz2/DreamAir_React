6d5febf8f6f3c1911dddd00b47809c0c674d5d75
wwwalohacampus <h850415@naver.com>
2023-12-06 16:08:08 +0900

    🚀 비행기loading, Search 에서 Container 로 state 끌어올리기

--- dreamair-app/src/components/admin/TicketSearch.jsx
+++ dreamair-app/src/components/admin/TicketSearch.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import Select from 'react-select';
 
-const TicketSearch = ({ flightNo, onSearch }) => {
+const TicketSearch = ({ onSearch }) => {
   const [selectedOption, setSelectedOption] = useState(null);
+  const [flightNo, setFlightNo] = useState(0);
 
   const options = [
     { value: '0', label: '선택' },
@@ -11,10 +12,21 @@ const TicketSearch = ({ flightNo, onSearch }) => {
   ];
 
   const handleChangeSelect = (selected) => {
+    console.log('＠＠＠＠＠＠＠＠ handleChangeSelect');
+    console.log('selected : ' + selected.value);      // selected 는 [Object] ➡  selected.value 가 값 (0,1,2)
     setSelectedOption(selected);
   };
 
+  const handleFlightNo = (e) => {
+    console.log(`input flightNo : ${e.target.value}`);
+    setFlightNo(e.target.value);
+  };
+
+
+
   const onSubmit = () => {
+    console.log('☆☆☆☆☆☆☆☆☆☆ TickeSearch');
+    console.log('flight No : ' + flightNo);
     const selectedValue = selectedOption ? selectedOption.value : '0';
     onSearch(flightNo, selectedValue);
   };
@@ -23,7 +35,10 @@ const TicketSearch = ({ flightNo, onSearch }) => {
       <div className="d-grid">
         <div className="d-flex">
           <label htmlFor="flightNo" className="" style={{ lineHeight: '50px' }}>항공기 번호 조회 : </label>
-          <input type="text" className="" style={{ lineHeight: '50px', width: '200px' }} id="filter" name="flightNo" placeholder='항공기 번호 입력'/>
+          <input type="text" className="" 
+                 style={{ lineHeight: '50px', width: '200px' }} id="filter" name="flightNo" placeholder='항공기 번호 입력'
+                 onChange={ handleFlightNo }
+                 />
           <Select
             className="select"
             id="searchType"

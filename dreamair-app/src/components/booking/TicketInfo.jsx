d7f8b38fbe97d1523e021dd383ccc57215f1d300
yunazz2 <z____yn@naver.com>
2023-12-12 10:40:25 +0900

    ⛄ 1212_10시

--- dreamair-app/src/components/booking/TicketInfo.jsx
+++ dreamair-app/src/components/booking/TicketInfo.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const TicketInfo = ({ticketNo, viewTicketDetail}) => {
+const TicketInfo = ({ticketNo, viewTicketDetail, isLoading}) => {
 
-  const ticketInfo = viewTicketDetail.viewTicketDetail; // 예시로 viewTicketDetail 객체에서 viewTicketDetail 키의 값 추출
-  const userInfo = viewTicketDetail.userInfo; // 예시로 viewTicketDetail 객체에서 userInfo 키의 값 추출
+  const ticketInfo = viewTicketDetail?.viewTicketDetail || [];
+const userInfo = viewTicketDetail?.userInfo || {};
   
   return (
     <div className="container p-4">
@@ -13,51 +13,59 @@ const TicketInfo = ({ticketNo, viewTicketDetail}) => {
         <img src="/img/ticket_page_ticket.png" alt="ticket_page_ticket.png" style={{ width: '50px', height: '50px' }} />
       </div>
 
+      { isLoading && 
+        <div>
+            <img src="/img/loading.webp" alt="loading" />
+        </div> 
+      }
       <h3>• 티켓 정보</h3>
+      {!isLoading && viewTicketDetail && ticketInfo.length > 0 && (
+        <>
+          <table className="table table-striped table-hover table-bordered text-center align-middle">
+            <thead>
+              <tr className="table-primary">
+                <th>탑승권 번호</th>
+                <th>출발지</th>
+                <th>도착지</th>
+                <th>출발일</th>
+                <th>선택 좌석</th>
+              </tr>
+            </thead>
 
-      <table className="table table-striped table-hover table-bordered text-center align-middle">
-        <thead>
-          <tr className="table-primary">
-            <th>탑승권 번호</th>
-            <th>출발지</th>
-            <th>도착지</th>
-            <th>출발일</th>
-            <th>선택 좌석</th>
-          </tr>
-        </thead>
+            <tbody>
+              <tr>
+                <td align='center'>{ticketInfo[0]?.ticketNo}</td>
+                <td align='center'>{ticketInfo[0]?.departure}</td>
+                <td align='center'>{ticketInfo[0]?.destination}</td>
+                <td align='center'>{ticketInfo[0]?.departureDate}</td>
+                <td align='center'>{ticketInfo[0]?.seatNo}</td>
+              </tr>
+            </tbody>
+          </table>
 
-        <tbody>
-          <tr>
-            <td align='center'>{ticketInfo[0]?.ticketNo}</td>
-            <td align='center'>{ticketInfo[0]?.departure}</td>
-            <td align='center'>{ticketInfo[0]?.destination}</td>
-            <td align='center'>{ticketInfo[0]?.departureDate}</td>
-            <td align='center'>{ticketInfo[0]?.seatNo}</td>
-          </tr>
-        </tbody>
-      </table>
+          <br/><br/><br/>
 
-      <br/><br/><br/>
+          <h3>• 예매자 정보</h3>
 
-      <h3>• 예매자 정보</h3>
+          <table className="table table-striped table-hover table-bordered text-center align-middle">
+            <thead>
+              <tr className="table-primary">
+                <th>예매자 명</th>
+                <th>핸드폰 번호</th>
+                <th>이메일</th>
+              </tr>
+            </thead>
 
-      <table className="table table-striped table-hover table-bordered text-center align-middle">
-        <thead>
-          <tr className="table-primary">
-            <th>예매자 명</th>
-            <th>핸드폰 번호</th>
-            <th>이메일</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <td align='center'>{userInfo.name}</td>
-            <td align='center'>{userInfo.phone}</td>
-            <td align='center'>{userInfo.email}</td>
-          </tr>
-        </tbody>
-      </table>
+            <tbody>
+              <tr>
+                <td align='center'>{userInfo.name}</td>
+                <td align='center'>{userInfo.phone}</td>
+                <td align='center'>{userInfo.email}</td>
+              </tr>
+            </tbody>
+          </table>
+        </>
+      )}
     </div>
   )
 }

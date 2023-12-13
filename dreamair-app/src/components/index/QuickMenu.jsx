8aab5d82acd6ae5db2c6db98a57372970940f3ee
seulgi9256 <sgij56@gmail.com>
2023-12-13 09:27:53 +0900

    버튼 hover 수정

--- dreamair-app/src/components/index/QuickMenu.jsx
+++ dreamair-app/src/components/index/QuickMenu.jsx
@@ -14,13 +14,13 @@ const QuickMenu = () => {
               <Link href="#"><img src="/img/booking.png" alt="예약 조회" />예약 조회</Link>
             </div>
             <div className="btn_search btn_search3">
-              <Link href="#"><img src="/img/Checkin.png" alt="체크인"/>체크인</Link>
+              <Link href="/user/checkin/user"><img src="/img/Checkin.png" alt="체크인"/>체크인</Link>
             </div>
             <div className="btn_search btn_search4">
-              <Link href="#"><img src="/img/search.png" alt="출도착 조회" />출도착 조회</Link>
+              <Link href="/user/productFlightList"><img src="/img/search.png" alt="출도착 조회" />출도착 조회</Link>
             </div>
             <div className="btn_search btn_search5">
-              <Link href="#"><img src="/img/bus.png" alt="공항버스 예약" />공항버스 예약</Link>
+              <Link href="/bus"><img src="/img/bus.png" alt="공항버스 예약" />공항버스 예약</Link>
             </div>
           </div>
       </div>
